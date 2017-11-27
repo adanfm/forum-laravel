9f478f9ec83e45b0cb02fdb9b75f282b940a6416
Adan Felipe Medeiros <adanfm@MacBook-Air-de-Adan.local>
2017-11-27 17:12:01 -0200

    threares e replies: migrations, seeds, model e controllers

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,30 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     static $password;
-
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => implode(' ', $faker->paragraphs),
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'thread_id' => function () {
+            return factory(App\Thread::class)->create()->id;
+        }
     ];
 });
